1e75c6868fdd18e058680915ee7407af6118b9f8
hamzasgh1998 <49130105+hamzasgh1998@users.noreply.github.com>
2021-11-17 16:34:31 +0100

    commit finale

---
+++ src/test/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests.java
@@ -0,0 +1,20 @@
+package tn.esprit.spring;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootTest
+public class TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests {
+
+	
+	@Test
+	public void contextLoads(){
+		
+		
+	}
+
+
+}
