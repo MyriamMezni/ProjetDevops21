dcfbb3f96baa098e3763646d2b5dcf1ceb74c174
Myriam mezni <49035547+MyriamMezni@users.noreply.github.com>
2021-11-17 12:12:29 +0100

    teste2

---
+++ src/test/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests.java
@@ -0,0 +1,15 @@
+package tn.esprit.spring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+
+@SpringBootTest
+public class TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests {
+
+	
+	@Test
+  void contextLoads(){
+	  
+  }
+
+}
