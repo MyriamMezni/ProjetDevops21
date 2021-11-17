f992157dde1249f9928cd17501da6b3d2ad98750
Myriam mezni <49035547+MyriamMezni@users.noreply.github.com>
2021-11-17 12:42:09 +0100

    teste2

--- src/test/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests.java
@@ -2,15 +2,18 @@ package tn.esprit.spring;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import tn.esprit.spring.test.EmployeTest;
 
 import org.junit.Test;
 
 @SpringBootTest
 class TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests {
 
+	EmployeTest ES;
 	@Test
   void contextLoads(){
-	
+	  ES.ajouterContrat();
+	  ES.ajouterEmploye();
 	   
   }
 
