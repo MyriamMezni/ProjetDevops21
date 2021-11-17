5692a12d8550e3c104978efdb03ae2c7713aab8f
Myriam mezni <49035547+MyriamMezni@users.noreply.github.com>
2021-11-17 12:16:50 +0100

    teste2

--- src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
+++ src/main/java/tn/esprit/spring/TimesheetSpringBootCoreDataJpaMvcRest1Application.java
@@ -1,8 +1,11 @@
 package tn.esprit.spring;
 
+import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import tn.esprit.spring.test.EmployeTest;
 
 @SpringBootApplication
 public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
@@ -13,4 +16,11 @@ public class TimesheetSpringBootCoreDataJpaMvcRest1Application {
 		SpringApplication.run(TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests.class, args);
 	}
 
+	EmployeTest ES;
+	@Test
+  void contextLoads(){
+	  ES.ajouterContrat();
+	  ES.ajouterEmploye();
+	   
+  }
 }
